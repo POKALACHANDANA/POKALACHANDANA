8d6bb6603fdd45c136b3ec9cc704f2cf1419368c
POKALACHANDANA <159169447+POKALACHANDANA@users.noreply.github.com>
2024-05-01 09:53:12 +0530

    audio_rev.py

--- week-8/audio_rev.py
+++ week-8/audio_rev.py
@@ -1,4 +1,4 @@
 from scipy.io import wavfile
-(fs,x)=wavfile.read('/home/rguktvalley/CL LAB/week-8/sample-file-1.wav')
+(fs,x)=wavfile.read('np.wav')
 x_reverse=x[::-1]
-wavfile.write('npy.wave',fs,x_reverse)
+wavfile.write('n.wave',fs,x_reverse)
