25864471cbce8a1da9db0b9420324cbe7c07736f
POKALACHANDANA <159169447+POKALACHANDANA@users.noreply.github.com>
2024-05-01 09:56:45 +0530

    revaud.m

--- week-8/revaud.m
+++ week-8/revaud.m
@@ -1,5 +1,5 @@
-inputfile='/home/rguktvalley/CL LAB/week-8/sample-file-1 (copy).wav'
-outputfile='/home/rguktvalley/CL LAB/week-8/sample-file-1 (copy).wav'
+inputfile='npy.wav'
+outputfile='npy.wav'
 [y,fs]=audioread(inputfile)
 reversedata=flipup(y)
 audiowrite(outputfile,reverseddata,fs)
